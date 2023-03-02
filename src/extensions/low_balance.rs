f5ea188fbba89aa6f8fb2d4f4800fbede33018f8
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-03-02 15:27:45 +0100

    Move poll module to the top level

--- src/extensions/low_balance.rs
+++ src/extensions/low_balance.rs
@@ -34,14 +34,13 @@ const XMLNS: &str = "http://www.verisign.com/epp/lowbalance-poll-1.0";
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::message::poll::MessagePollResponse;
-    use crate::message::MessagePoll;
+    use crate::poll::{MessagePoll, MessagePollResponse};
     use crate::response::ResultCode;
     use crate::tests::{response_from_file, CLTRID, SVTRID};
 
     #[test]
     fn low_balance() {
-        let object = response_from_file::<MessagePoll>("response/message/poll_low_balance.xml");
+        let object = response_from_file::<MessagePoll>("response/poll/poll_low_balance.xml");
         dbg!(&object);
 
         let low_balance = match object.res_data() {
