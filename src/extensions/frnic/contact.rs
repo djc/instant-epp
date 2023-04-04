4c67212824459776fdc83236bade1df93ff9592d
Kim Minh Kaplan <kimminh.kaplan+git@afnic.fr>
2023-04-04 12:23:20 +0000

    [FRNIC] Extension to create corporation and non profit contact

--- src/extensions/frnic/contact.rs
+++ src/extensions/frnic/contact.rs
@@ -1,15 +1,15 @@
 //! Types for EPP FRNIC contact requests
+use instant_xml::{FromXml, Id, ToXml};
 use std::borrow::Cow;
-use instant_xml::{FromXml, ToXml};
 
 use crate::contact::create::ContactCreate;
 use crate::request::{Extension, Transaction};
 
-use super::{Ext, Create, XMLNS};
+use super::{Create, Ext, XMLNS};
 
 impl<'a> Transaction<Ext<Create<FrnicContactCreatePp<'a>>>> for ContactCreate<'a> {}
 
-impl <'a> Extension for Ext<Create<FrnicContactCreatePp<'a>>> {
+impl<'a> Extension for Ext<Create<FrnicContactCreatePp<'a>>> {
     type Response = ();
 }
 
@@ -17,5 +17,82 @@ impl <'a> Extension for Ext<Create<FrnicContactCreatePp<'a>>> {
 #[xml(rename = "contact", ns(XMLNS))]
 pub struct FrnicContactCreatePp<'a> {
     #[xml(rename = "firstName")]
-    pub first_name: Cow<'a, str>
+    pub first_name: Cow<'a, str>,
+}
+
+impl<'a> Transaction<Ext<Create<FrnicContactCreateCorporation<'a>>>> for ContactCreate<'a> {}
+
+impl<'a> Extension for Ext<Create<FrnicContactCreateCorporation<'a>>> {
+    type Response = ();
+}
+
+#[derive(Debug, ToXml)]
+#[xml(rename = "contact", ns(XMLNS))]
+pub struct FrnicContactCreateCorporation<'a> {
+    pub legal_entity: LegalEntityInfos<'a>,
+}
+
+#[derive(Debug, ToXml)]
+#[xml(rename = "legalEntityInfos", ns(XMLNS))]
+pub struct LegalEntityInfos<'a> {
+    #[xml(rename = "legalStatus")]
+    pub legal_status: LegalStatus<'a>,
+    pub siren: Option<Cow<'a, str>>,
+    pub vat: Option<Cow<'a, str>>,
+    pub trademark: Option<Cow<'a, str>>,
+    pub asso: Option<Asso<'a>>,
+    pub duns: Option<Cow<'a, str>>,
+    pub local: Option<Cow<'a, str>>,
+}
+
+#[derive(Debug)]
+pub enum LegalStatus<'a> {
+    Company,
+    NonProfit,
+    Other(Cow<'a, str>),
+}
+
+impl<'a> ToXml for LegalStatus<'a> {
+    fn serialize<W: core::fmt::Write + ?Sized>(
+        &self,
+        field: Option<Id<'_>>,
+        serializer: &mut instant_xml::Serializer<W>,
+    ) -> Result<(), instant_xml::Error> {
+        let field = field.expect("field for LegalStatus has a name");
+        let (s, data) = match self {
+            LegalStatus::Company => ("company", ""),
+            LegalStatus::NonProfit => ("association", ""),
+            LegalStatus::Other(data) => ("other", data.as_ref()),
+        };
+        let prefix = serializer.write_start(field.name, field.ns)?;
+        debug_assert_eq!(prefix, None);
+        serializer.write_attr("s", XMLNS, s)?;
+        if data.is_empty() {
+            serializer.end_empty()?;
+        } else {
+            serializer.end_start()?;
+            data.serialize(None, serializer)?;
+            serializer.write_close(prefix, field.name)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, ToXml)]
+#[xml(rename = "asso", ns(XMLNS))]
+pub struct Asso<'a> {
+    pub waldec: Option<Cow<'a, str>>,
+    pub decl: Option<Cow<'a, str>>,
+    pub publ: Option<Publication<'a>>,
+}
+
+#[derive(Debug, ToXml)]
+#[xml(rename = "publ", ns(XMLNS))]
+pub struct Publication<'a> {
+    #[xml(attribute)]
+    pub page: u32,
+    #[xml(attribute)]
+    pub announce: u32,
+    #[xml(direct)]
+    pub date: Cow<'a, str>,
 }
--- src/extensions/frnic/mod.rs
+++ src/extensions/frnic/mod.rs
@@ -16,23 +16,76 @@ pub struct Create<T> {
     pub data: T,
 }
 
-pub fn frnic_contact_create_physical_person<'a>(
-    first_name: &'a str
-) -> Ext<Create<contact::FrnicContactCreatePp<'a>>> {
+pub fn frnic_contact_create_physical_person(
+    first_name: &str,
+) -> Ext<Create<contact::FrnicContactCreatePp<'_>>> {
     Ext {
         data: Create {
             data: contact::FrnicContactCreatePp {
                 first_name: first_name.into(),
-            }
-        }
+            },
+        },
+    }
+}
+
+pub fn frnic_contact_create_company<'a>(
+    siren: Option<&'a str>,
+    vat: Option<&'a str>,
+    trademark: Option<&'a str>,
+    duns: Option<&'a str>,
+    local: Option<&'a str>,
+) -> Ext<Create<contact::FrnicContactCreateCorporation<'a>>> {
+    Ext {
+        data: Create {
+            data: contact::FrnicContactCreateCorporation {
+                legal_entity: contact::LegalEntityInfos {
+                    legal_status: contact::LegalStatus::Company,
+                    siren: siren.map(|s| s.into()),
+                    vat: vat.map(|v| v.into()),
+                    trademark: trademark.map(|t| t.into()),
+                    asso: None,
+                    duns: duns.map(|d| d.into()),
+                    local: local.map(|l| l.into()),
+                },
+            },
+        },
+    }
+}
+
+pub fn frnic_contact_create_non_profit<'a>(
+    waldec: Option<&'a str>,
+    decl: Option<&'a str>,
+    publication: Option<contact::Publication<'a>>,
+) -> Ext<Create<contact::FrnicContactCreateCorporation<'a>>> {
+    Ext {
+        data: Create {
+            data: contact::FrnicContactCreateCorporation {
+                legal_entity: contact::LegalEntityInfos {
+                    legal_status: contact::LegalStatus::NonProfit,
+                    siren: None,
+                    vat: None,
+                    trademark: None,
+                    asso: Some(contact::Asso {
+                        waldec: waldec.map(|w| w.into()),
+                        decl: decl.map(|d| d.into()),
+                        publ: publication,
+                    }),
+                    duns: None,
+                    local: None,
+                },
+            },
+        },
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::frnic_contact_create_physical_person;
-    use crate::contact::{Address, PostalInfo, Voice};
+    use super::{
+        contact::Publication, frnic_contact_create_company, frnic_contact_create_non_profit,
+        frnic_contact_create_physical_person,
+    };
     use crate::contact::create::ContactCreate;
+    use crate::contact::{Address, PostalInfo, Voice};
     use crate::tests::assert_serialized;
 
     #[test]
@@ -55,10 +108,102 @@ mod tests {
                 ),
             ),
             Some(Voice::new("+33.1234567890")),
-            "Afn-12345678"
+            "Afn-12345678",
         );
         assert_serialized(
             "request/extensions/frnic_create_contact_physical_person.xml",
+            (&object, &frnic_contact),
+        );
+    }
+
+    #[test]
+    fn contact_create_corporation() {
+        // Guide d'intégration tecnhique, Septembre 2022, page 25.
+        let frnic_contact = frnic_contact_create_company(None, None, None, None, None);
+        let object = ContactCreate::new(
+            "XXXXXXX",
+            "test@test.fr",
+            PostalInfo::new(
+                "loc",
+                "SARL DUPONT",
+                None,
+                Address::new(
+                    &["1 Rue des coquelicots"],
+                    "Paris",
+                    None,
+                    Some("75000"),
+                    "FR".parse().unwrap(),
+                ),
+            ),
+            Some(Voice::new("+33.1234567890")),
+            "Afn-123456",
+        );
+        assert_serialized(
+            "request/extensions/frnic_create_contact_corporation.xml",
+            (&object, &frnic_contact),
+        );
+    }
+
+    #[test]
+    fn contact_create_corporation_with_siren() {
+        // Guide d'intégration tecnhique, Septembre 2022, page 26.
+        let frnic_contact = frnic_contact_create_company(Some("123456789"), None, None, None, None);
+        let object = ContactCreate::new(
+            "XXXX0000",
+            "test@test.fr",
+            PostalInfo::new(
+                "loc",
+                "SARL DUPONT SIREN",
+                None,
+                Address::new(
+                    &["1 Rue des Sirenes"],
+                    "Paris",
+                    None,
+                    Some("75000"),
+                    "FR".parse().unwrap(),
+                ),
+            ),
+            Some(Voice::new("+33.1234567890")),
+            "Afn-123456",
+        );
+        assert_serialized(
+            "request/extensions/frnic_create_contact_siren.xml",
+            (&object, &frnic_contact),
+        );
+    }
+
+    #[test]
+    fn contact_create_non_profit() {
+        // Guide d'intégration tecnhique, Septembre 2022, page 33.
+        let frnic_contact = frnic_contact_create_non_profit(
+            None,
+            Some("2011-05-02"),
+            Some(Publication {
+                announce: 123456,
+                page: 15,
+                date: "2011-05-07".into(),
+            }),
+        );
+        let object = ContactCreate::new(
+            "XXXX0000",
+            "test@test.fr",
+            PostalInfo::new(
+                "loc",
+                "Dupont JO",
+                None,
+                Address::new(
+                    &["1 Rue des Fleurs"],
+                    "Paris",
+                    None,
+                    Some("75000"),
+                    "FR".parse().unwrap(),
+                ),
+            ),
+            Some(Voice::new("+33.1234567890")),
+            "Afn-123456",
+        );
+        assert_serialized(
+            "request/extensions/frnic_create_contact_non_profit.xml",
             (&object, &frnic_contact),
         );
     }
