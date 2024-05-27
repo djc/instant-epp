20e7acec3bb97b611ac4d93ccb99332cf3a43f37
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-05-27 11:10:16 +0200

    Use enum to represent RGP status values

--- src/extensions/rgp/poll.rs
+++ src/extensions/rgp/poll.rs
@@ -3,6 +3,8 @@
 use chrono::{DateTime, Utc};
 use instant_xml::FromXml;
 
+use super::RgpStatus;
+
 /// RGP request status
 #[derive(Debug, FromXml)]
 #[xml(rename = "pollData", ns(XMLNS), rename_all = "camelCase")]
@@ -13,19 +15,11 @@ pub struct RgpPollData {
     pub report_due_date: DateTime<Utc>,
 }
 
-/// Type that represents the `<rgpStatus>` tag for domain rgp restore request response
-#[derive(Debug, FromXml)]
-#[xml(rename = "rgpStatus", ns(XMLNS))]
-pub struct RgpStatus {
-    /// The domain RGP status
-    #[xml(rename = "s", attribute)]
-    pub status: String,
-}
-
-const XMLNS: &str = "http://www.verisign.com/epp/rgp-poll-1.0";
+pub(super) const XMLNS: &str = "http://www.verisign.com/epp/rgp-poll-1.0";
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::poll::{Poll, PollData};
     use crate::tests::response_from_file;
 
@@ -37,6 +31,6 @@ mod tests {
         };
 
         assert_eq!(data.name, "EXAMPLE.COM");
-        assert_eq!(data.rgp_status.status, "pendingRestore");
+        assert_eq!(data.rgp_status, RgpStatus::PendingRestore);
     }
 }
