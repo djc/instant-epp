82f9ef79ac1df5c994c8ab87ba12ac3456a6c567
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-05-27 10:57:20 +0200

    Move rgp module into a separate file

---
+++ src/extensions/rgp/mod.rs
@@ -0,0 +1,8 @@
+//! Mapping for the Registry Grace Period extension
+//!
+//! As described in [RFC 3915](https://tools.ietf.org/html/rfc3915).
+
+pub mod report;
+pub mod request;
+
+pub const XMLNS: &str = "urn:ietf:params:xml:ns:rgp-1.0";
--- src/lib.rs
+++ src/lib.rs
@@ -52,21 +52,11 @@ pub mod xml;
 
 pub mod extensions {
     pub mod consolidate;
+    pub mod frnic;
     pub mod low_balance;
     pub mod namestore;
-
-    /// Mapping for the Registry Grace Period extension
-    ///
-    /// As described in [RFC 3915](https://tools.ietf.org/html/rfc3915).
-    pub mod rgp {
-        pub mod report;
-        pub mod request;
-
-        pub const XMLNS: &str = "urn:ietf:params:xml:ns:rgp-1.0";
-    }
+    pub mod rgp;
     pub mod secdns;
-
-    pub mod frnic;
 }
 
 pub use client::EppClient;
